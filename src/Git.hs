62d29cc5de84b3b55168847a915c7905b5af30c2
Sakari Jokinen <sakariij@gmail.com>
2011-02-25 04:38:41 +0200

    first test passes

--- src/Git.hs
+++ src/Git.hs
@@ -1,4 +1,14 @@
-module Git where
+module Git (Repository, open) where
 import Bindings.Libgit2
+import Foreign
+import Foreign.C.String
+import Control.Monad(when)
 
-open = undefined
+newtype Repository = Repository (Ptr C'git_repository)
+
+open::FilePath -> IO (Maybe Repository)
+open path = alloca $ \repoPtr -> do
+  withCString path $ \pathPtr -> do
+    r <- c'git_repository_open repoPtr pathPtr
+    if  (r < 0) then return Nothing
+      else (Just . Repository) `fmap` peek repoPtr

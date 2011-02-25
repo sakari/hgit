96045294a001d7802b111f0a52190bde47bb7d3d
Sakari Jokinen <sakariij@gmail.com>
2011-02-25 09:30:59 +0200

    commit lookup started

--- src/Git.hs
+++ src/Git.hs
@@ -1,14 +1,39 @@
 module Git (Repository
+           , Commit
+           , Oid
+           , oidMkStr
            , open
            , initialize
-           , free) where
+           , free
+           , commitLookup) where
 import Bindings.Libgit2
 import Foreign hiding (free)
 import Foreign.C.String
 import Foreign.C.Types
 import Control.Monad(when)
 
+newtype Commit = Commit { commitPtr::Ptr C'git_commit }
+newtype Oid = Oid { oidPtr::Ptr C'git_oid }
 newtype Repository = Repository { repoPtr :: Ptr C'git_repository }
+
+instance Show Oid where
+  show oid = "{ Oid: " ++ oidFmt oid ++ " }"
+
+oidFmt::Oid -> String
+oidFmt Oid { oidPtr } = unsafePerformIO $ do
+  allocaBytes c'GIT_OID_HEXSZ $ \output -> do 
+    c'git_oid_fmt output oidPtr
+    peekCStringLen (output, c'GIT_OID_HEXSZ)
+
+oidMkStr::String -> Maybe Oid
+oidMkStr hex 
+  | length hex /= 40 = Nothing
+  | otherwise = unsafePerformIO $ do
+    withCString hex $ \c'hex -> do
+      oidPtr <- malloc
+      r <- c'git_oid_mkstr oidPtr c'hex 
+      if (r < 0) then return Nothing
+        else return $ Just $ Oid oidPtr
 
 open::FilePath -> IO (Maybe Repository)
 open path = alloca $ \repoPtr -> do
@@ -26,6 +51,13 @@ initialize path is_bare = alloca $ \repoPtr -> do
 
 free::Repository -> IO ()
 free = c'git_repository_free . repoPtr
+
+commitLookup::Repository -> Oid -> IO (Maybe Commit)
+commitLookup Repository { repoPtr } Oid { oidPtr } = do
+  alloca $ \commitPtr -> do
+    r <- c'git_commit_lookup commitPtr repoPtr oidPtr
+    if (r < 0) then return Nothing
+      else (Just . Commit) `fmap` peek commitPtr 
 
 c'bool::Bool -> CInt
 c'bool True = 1
--- tests/Test/TestGit.hs
+++ tests/Test/TestGit.hs
@@ -25,16 +25,25 @@ inSandboxDirectory sandboxLocation io = bracket enterSandbox exitSandbox (const 
 exceptionToFalse::SomeException -> IO Bool
 exceptionToFalse _ = return False
         
+given_a_repository block =                     
+  sandbox $ bracket createRepo freeRepo block
+    where
+      createRepo = 
+        fromJust `fmap` Git.initialize "repository" False
+      freeRepo repo = Git.free repo
+
 sandbox::IO Bool -> Bool
 sandbox io = 
   unsafePerformIO $ 
-    inSandboxDirectory "sandbox" $
-      io `catch` exceptionToFalse
+    inSandboxDirectory "sandbox" io
 
 newtype RepoName = RepoName FilePath 
                  deriving Show
 instance Arbitrary RepoName where
   arbitrary = RepoName `fmap` listOf1 (choose ('a', 'z'))
+  
+instance Arbitrary Git.Oid where  
+  arbitrary = (fromJust . Git.oidMkStr) `fmap` vectorOf 40 (oneof [choose ('a', 'f'), choose('0', '9')])
   
 prop_opening_nonexistant_repo_fails (RepoName repo) =
   sandbox $ isNothing `fmap` Git.open repo
@@ -53,12 +62,17 @@ prop_opening_an_initialized_repo_succeeds (RepoName name) =
     Just repo <- Git.initialize name False
     isJust `fmap` Git.open (joinPath [name, ".git"]) 
 
+prop_lookup_nonexisting_commit sha1 =
+  given_a_repository $ \repo -> do
+    isNothing `fmap` Git.commitLookup repo sha1
+        
 run title prop = do
   print $ "######### Test: " ++ title
   quickCheckResult prop
 
-tests = [run "open nonexisting repo" prop_opening_nonexistant_repo_fails
+tests = [ run "open nonexisting repo" prop_opening_nonexistant_repo_fails
         , run "open initialized repo" prop_opening_an_initialized_repo_succeeds
         , run "initialize a repo" prop_initializing_a_normal_repository_succeeds
         , run "free created repo" prop_closing_an_initialized_repo
+        , run "lookup a non existing commit" prop_lookup_nonexisting_commit
         ]

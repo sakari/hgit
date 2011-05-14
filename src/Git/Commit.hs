42c6c669e814977bb171d9d821de9990b17d9a56
Sakari Jokinen <sakariij@gmail.com>
2011-05-14 16:22:45 +0300

    wip

--- src/Git/Commit.hs
+++ src/Git/Commit.hs
@@ -1,5 +1,6 @@
 module Git.Commit where
-import Foreign.ForeignPtr
+import Foreign.ForeignPtr hiding (newForeignPtr)
+import Foreign.Concurrent
 import Foreign.Ptr
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
@@ -25,9 +26,11 @@ lookup repo oid = do
         c'git_commit_lookup ptr repo_ptr oid_ptr `handle_git_return` wrap ptr repo_ptr oid_ptr
   where
     wrap ptr repo_ptr oid_ptr = do
-      wrapped_commit_ptr <- peek ptr $ newForeignPtr c'git_commit_close
+      wrapped_commit_ptr <- do 
+        p <- peek ptr
+        newForeignPtr p $ c'git_commit_close p
       return $ Commit { commit_ptr = wrapped_commit_ptr, commit_repo_ptr = repository_ptr repo }
-
+  
 type Ref = String
 type Message = String
 type OidT a = Oid
@@ -37,7 +40,7 @@ type Committer = Signature
 data Signature = Signature { signature_ptr::ForeignPtr C'git_signature }
   
 withMaybeCString::Maybe String -> (CString -> IO a) -> IO a
-withMaybeCString string action = maybe string (action nullPtr) (flip withCString action)
+withMaybeCString string action = maybe (action nullPtr) (flip withCString action) string
 
 withForeignPtrs::[ForeignPtr a] -> ([Ptr a] -> IO b) -> IO b
 withForeignPtrs fptrs action = go fptrs [] 
@@ -48,7 +51,7 @@ withForeignPtrs fptrs action = go fptrs []
 create::Repository -> Maybe Ref -> Author -> Committer -> Message -> OidT Tree -> [OidT Commit] -> Result (OidT Commit) 
 create repo ref author committer message tree parents = do
   oid_fptr <- mallocForeignPtr
-  withForeignPtr oid_fptr $ \oid_ptr -> do
+  withForeignPtr oid_fptr $ \result_oid_ptr -> do
     withForeignPtr (repository_ptr repo) $ \repo_ptr -> do
       withMaybeCString ref $ \ref_ptr -> do
         withForeignPtr (signature_ptr author) $ \auth_ptr -> do
@@ -56,5 +59,6 @@ create repo ref author committer message tree parents = do
             withCString message $ \message_ptr -> do
               withForeignPtr (oid_ptr tree) $ \tree_ptr -> do
                 withForeignPtrs (map oid_ptr parents) $ \parent_oid_ptrs -> do
-                  withArray parent_oid_ptrs $ \parent_oid_array -> do
-                    c'git_commit_create oid_ptr repo_ptr ref_ptr auth_ptr committer_ptr message_ptr tree_ptr (length parents) parent_oid_array `handle_git_return` Oid oid_fptr
+                  parent_oids <- mapM peek parent_oid_ptrs
+                  withArray parent_oids $ \parent_oid_array -> do
+                    c'git_commit_create result_oid_ptr repo_ptr ref_ptr auth_ptr committer_ptr message_ptr tree_ptr (fromIntegral $ length parents) parent_oid_array `handle_git_return` (return $ Oid oid_fptr)

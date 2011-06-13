74dcb01e4b1ac1f0194f32bf121a1bedb9406421
Sakari Jokinen <sakariij@gmail.com>
2011-06-13 20:40:57 +0300

    increase range for attributes

--- src/Git/Types.hs
+++ src/Git/Types.hs
@@ -87,5 +87,5 @@ newtype Attributes = Attributes { attributes::Int }
                      deriving (Eq, Show, Ord, Num, Integral, Real, Enum)
 
 instance Bounded Attributes where
-  maxBound = 0o77777 -- 0o777777
+  maxBound = 0o777777
   minBound = 0
